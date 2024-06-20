ae4184d0f17355218aefc1c7ced35e461665cc35
Levy Lv <golvwei@gmail.com>
2024-06-20 23:44:05 +0800

    Add cuid (#6)

--- src/books/schemas.py
+++ src/books/schemas.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel
 from datetime import datetime, date
-import uuid
 
 
 class BookModel(BaseModel):
-    id: uuid.UUID
+    id: str
     title: str
     author: str
     publisher: str
