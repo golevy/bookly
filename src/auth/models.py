0ade9fde40b5307d0dbe8f286c1848b1f7dc0fc2
Levy <golvwei@gmail.com>
2024-06-28 00:28:58 +0800

    Add database migrations

--- src/auth/models.py
+++ src/auth/models.py
@@ -17,26 +17,26 @@ class User(SQLModel, table=True):
     )
     username: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
     email: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
-    first_name = Field(sa_column=Column(pg.VARCHAR, nullable=False))
-    last_name = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    first_name: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    last_name: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
     is_verified: bool = Field(
         default=False, sa_column=Column(pg.BOOLEAN, nullable=False)
     )
     created_at: datetime = Field(
-        default_factory=datetime.now(timezone.utc),
+        default_factory=lambda: datetime.now(timezone.utc),
         sa_column=Column(
             pg.TIMESTAMP(timezone=True),
             nullable=False,
-            default=datetime.now(timezone.utc),
+            default=lambda: datetime.now(timezone.utc),
         ),
     )
     updated_at: datetime = Field(
-        default_factory=datetime.now(timezone.utc),
+        default_factory=lambda: datetime.now(timezone.utc),
         sa_column=Column(
             pg.TIMESTAMP(timezone=True),
             nullable=False,
-            default=datetime.now(timezone.utc),
-            onupdate=datetime.now(timezone.utc),
+            default=lambda: datetime.now(timezone.utc),
+            onupdate=lambda: datetime.now(timezone.utc),
         ),
     )
 
