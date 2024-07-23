4f3f048953f8ee45b2db38b0ef9cd6347d4420d2
Golevy <golvwei@gmail.com>
2024-07-23 23:57:20 +0800

    Add auth service (#12)

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,9 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from src.db.main import get_session
 from .schemas import UserCreateModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from .service import UserService
+from fastapi.exceptions import HTTPException
+from .schemas import UserModel
 
 auth_router = APIRouter()
+user_service = UserService()
 
 
-@auth_router.post("/signup")
-async def create_user_account(user: UserCreateModel):
-    pass
+@auth_router.post(
+    "/signup", response_model=UserModel, status_code=status.HTTP_201_CREATED
+)
+async def create_user_account(
+    user_data: UserCreateModel, session: AsyncSession = Depends(get_session)
+):
+    email = user_data.email
+    user_exists = await user_service.user_exists(email, session)
+    if user_exists:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User with email already exists",
+        )
+    new_user = await user_service.create_user(user_data, session)
+
+    return new_user
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,7 +1,22 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 
 class UserCreateModel(BaseModel):
-    username: str = Field(min_length=1, max_length=10)
+    first_name: str = Field(max_length=25)
+    last_name: str = Field(max_length=25)
+    username: str = Field(min_length=1, max_length=50)
     email: str = Field(max_length=40)
     password: str = Field(min_length=6)
+
+
+class UserModel(BaseModel):
+    id: str
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    updated_at: datetime
