e35e25e106f80de600fb7ec1c02e8e4a1ff2f853
Levy <golvwei@gmail.com>
2024-07-24 23:32:12 +0800

    Create access and refresh token

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -20,3 +20,8 @@ class UserModel(BaseModel):
     password_hash: str = Field(exclude=True)
     created_at: datetime
     updated_at: datetime
+
+
+class UserLoginModel(BaseModel):
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
--- src/config.py
+++ src/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
