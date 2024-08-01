462eace9a8accec49c51755fe0c798b14e15ad43
Golevy <golvwei@gmail.com>
2024-08-01 23:44:18 +0800

    Revoke access token using redis (#16)

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -8,7 +8,8 @@ from fastapi.exceptions import HTTPException
 from .schemas import UserModel
 from .utils import create_access_token, verify_password
 from datetime import timedelta, datetime
-from .dependencies import RefreshTokenBearer
+from .dependencies import RefreshTokenBearer, AccessTokenBearer
+from src.db.redis import add_jti_to_blocklist
 
 auth_router = APIRouter()
 user_service = UserService()
@@ -90,4 +91,14 @@ async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer(
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
         detail="Invalid or expired refresh token",
+    )
+
+
+@auth_router.get("/logout")
+async def revoke_token(token_details: dict = Depends(AccessTokenBearer())):
+    jti = token_details["jti"]
+    await add_jti_to_blocklist(jti)
+
+    return JSONResponse(
+        content={"message": "Logged out successfully"}, status_code=status.HTTP_200_OK
     )
