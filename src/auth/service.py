3634858ec514fd462e5bff275e334f59e0e9ddd9
Golevy <golvwei@gmail.com>
2024-08-14 17:22:10 +0800

    Reorganize the database models (#24)

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,4 +1,4 @@
-from .models import User
+from src.db.models import User
 from .schemas import UserCreateModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
--- src/db/models.py
+++ src/db/models.py
@@ -1,9 +1,8 @@
+from typing import List, Optional
 from sqlmodel import SQLModel, Field, Column, Relationship
-from src.books import models
 from cuid import cuid
-from typing import List
+from datetime import date, datetime, timezone
 import sqlalchemy.dialects.postgresql as pg
-from datetime import datetime, timezone
 
 
 class User(SQLModel, table=True):
@@ -47,9 +46,50 @@ class User(SQLModel, table=True):
             onupdate=lambda: datetime.now(timezone.utc),
         ),
     )
-    books: List["models.Book"] = Relationship(
+    books: List["Book"] = Relationship(
         back_populates="user", sa_relationship_kwargs={"lazy": "selectin"}
     )
 
     def __repr__(self) -> str:
         return f"<User {self.username}>"
+
+
+class Book(SQLModel, table=True):
+    __tablename__ = "book"
+
+    id: str = Field(
+        default_factory=cuid,
+        sa_column=Column(
+            pg.VARCHAR,
+            nullable=False,
+            primary_key=True,
+        ),
+    )
+    title: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    author: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    publisher: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    published_date: date = Field(sa_column=Column(pg.DATE, nullable=False))
+    page_count: int = Field(sa_column=Column(pg.INTEGER, nullable=False))
+    language: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    user_id: Optional[str] = Field(default=None, foreign_key="user.id")
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(
+            pg.TIMESTAMP(timezone=True),
+            nullable=False,
+            default=lambda: datetime.now(timezone.utc),
+        ),
+    )
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(
+            pg.TIMESTAMP(timezone=True),
+            nullable=False,
+            default=lambda: datetime.now(timezone.utc),
+            onupdate=lambda: datetime.now(timezone.utc),
+        ),
+    )
+    user: Optional["User"] = Relationship(back_populates="books")
+
+    def __repr__(self) -> str:
+        return f"<Book {self.title}>"
