59af573d14b10952be2d386d7944433a3ceffdf4
Levy <golvwei@gmail.com>
2024-08-28 12:08:56 +0800

    Add authorization middleware

--- src/middleware.py
+++ src/middleware.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 import time
 import logging
 
@@ -16,5 +18,20 @@ def register_middleware(app: FastAPI):
         processing_time = time.time() - start_time
         message = f"{request.client.host}:{request.client.port} - {request.method} - {request.url.path} - {response.status_code} - completed after {processing_time}s"
         print(message)
+
+        return response
+
+    @app.middleware("http")
+    async def authorization(request: Request, call_next):
+        if not "Authorization" in request.headers:
+            return JSONResponse(
+                content={
+                    "message": "Authorization header is missing",
+                    "resolution": "Please provide a valid token in the Authorization header",
+                },
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        response = await call_next(request)
 
         return response
