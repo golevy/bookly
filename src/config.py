b6a37a00d72b6d6d2a26b246b07feccf7819988f
Levy <golvwei@gmail.com>
2024-09-01 23:58:16 +0800

    Set up fastapi mail

--- src/config.py
+++ src/config.py
@@ -7,6 +7,16 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
 
     model_config = SettingsConfigDict(
         env_file=".env",
