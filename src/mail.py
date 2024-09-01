081923a1e92e51530b2ebf58c1f596c13648b738
Golevy <golvwei@gmail.com>
2024-09-01 23:58:52 +0800

    Set up fastapi mail (#39)

---
+++ src/mail.py
@@ -0,0 +1,17 @@
+from fastapi_mail import FastMail, ConnectionConfig
+from src.config import Config
+
+mail_config = ConnectionConfig(
+    MAIL_USERNAME=Config.MAIL_USERNAME,
+    MAIL_PASSWORD=Config.MAIL_PASSWORD,
+    MAIL_FROM=Config.MAIL_FROM,
+    MAIL_PORT=Config.MAIL_PORT,
+    MAIL_SERVER=Config.MAIL_SERVER,
+    MAIL_FROM_NAME=Config.MAIL_FROM_NAME,
+    MAIL_STARTTLS=Config.MAIL_STARTTLS,
+    MAIL_SSL_TLS=Config.MAIL_SSL_TLS,
+    USE_CREDENTIALS=Config.USE_CREDENTIALS,
+    VALIDATE_CERTS=Config.VALIDATE_CERTS,
+)
+
+mail = FastMail(config=mail_config)
