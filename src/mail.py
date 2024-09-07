5982a01a3a6270575c002aeb66aeb9c3a4271c94
Levy <golvwei@gmail.com>
2024-09-07 23:06:42 +0800

    Add template folder

--- src/mail.py
+++ src/mail.py
@@ -1,5 +1,8 @@
 from fastapi_mail import FastMail, ConnectionConfig
 from src.config import Config
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
 
 mail_config = ConnectionConfig(
     MAIL_USERNAME=Config.MAIL_USERNAME,
@@ -12,6 +15,11 @@ mail_config = ConnectionConfig(
     MAIL_SSL_TLS=Config.MAIL_SSL_TLS,
     USE_CREDENTIALS=Config.USE_CREDENTIALS,
     VALIDATE_CERTS=Config.VALIDATE_CERTS,
+    TEMPLATE_FOLDER=Path(BASE_DIR, "templates"),
 )
 
 mail = FastMail(config=mail_config)
+
+
+def create_message():
+    pass
