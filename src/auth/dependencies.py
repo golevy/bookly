cc5af96db5f7750b5a91aec5472342d6df76fe80
Golevy <golvwei@gmail.com>
2024-09-29 17:14:24 +0800

    Update role checker (#44)

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -14,6 +14,7 @@ from src.errors import (
     AccessTokenRequired,
     RefreshTokenRequired,
     InsufficientPermissions,
+    AccountNotVerified,
 )
 
 user_service = UserService()
@@ -74,6 +75,8 @@ class RoleChecker:
         self.allowed_roles = allowed_roles
 
     def __call__(self, current_user: User = Depends(get_current_user)) -> Any:
+        if not current_user.is_verified:
+            raise AccountNotVerified()
         if current_user.role in self.allowed_roles:
             return True
 
