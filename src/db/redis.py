f5e91644863e0cd3781132e4c0c4e4663eaa6368
Levy <golvwei@gmail.com>
2024-10-07 22:40:30 +0800

    Config celery

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,11 +1,9 @@
-from redis import asyncio as aioredis
 from src.config import Config
+import redis.asyncio as aioredis
 
 JTI_EXPIRY = 3600
 
-token_blocklist = aioredis.StrictRedis(
-    host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=0
-)
+token_blocklist = aioredis.from_url(Config.REDIS_URL)
 
 
 async def add_jti_to_blocklist(jti: str) -> None:
