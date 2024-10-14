b2aa8d2e96490e57431826b9dfb4bdff67f33893
Golevy <golvwei@gmail.com>
2024-10-14 15:29:33 +0800

    Add flower to monitor tasks (#53)

--- src/celery_tasks.py
+++ src/celery_tasks.py
@@ -11,3 +11,4 @@ def send_email(recipients: list[str], subject: str, body: str):
     message = create_message(recipients=recipients, subject=subject, body=body)
 
     async_to_sync(mail.send_message)(message)
+    print("Email sent successfully")
